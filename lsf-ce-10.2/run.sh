80a01cc11a98c664e278e26c8e3f4688dcd5dd32
Michael R. Crusoe <michael.crusoe@gmail.com>
2018-05-06 09:40:59 -0700

    closer

--- lsf-ce-10.2/run.sh
+++ lsf-ce-10.2/run.sh
@@ -3,18 +3,20 @@ user=$1
 useradd ${user}
 mkhomedir_helper ${user}
 find / -group docker -exec chgrp -hf --reference /var/run/docker.sock {} \;
-groupmod --gid $(stat --format %g /var/run/docker.sock) docker
+groupmod --non-unique --gid $(stat --format %g /var/run/docker.sock) docker
 usermod -a -G docker ${user}
-cp -r /home/lsfadmin/cwlexec-master /home/${user}
-chmod +777 /home/${user}/cwlexec-master -r
+cp -r /home/lsfadmin/cwlexec* /home/${user}
+chown ${user} /home/${user} /home/${user}/cwlexec*
+chmod +777 /home/${user}
+chmod +777 /home/${user}/cwlexec* -R
 source /opt/ibm/lsfsuite/lsf/conf/profile.lsf
 source <(head -n265 /start_lsf_ce.sh | tail -n +7)
 ROLE=master config_lsfce
 ROLE=master start_lsf
 lsid
 lshosts
-su lsfadmin -c 'source /opt/ibm/lsfsuite/lsf/conf/profile.lsf && \
+su ${user} -c 'source /opt/ibm/lsfsuite/lsf/conf/profile.lsf && \
 	/common-workflow-language-master/run_test.sh \
-	RUNNER=/home/lsfadmin/cwlexec-0.1/cwlexec \
-	--junit-xml=/home/lsfadmin/cwlexec-0.1.xml' || /bin/true
+	RUNNER=~/cwlexec-0.1/cwlexec \
+	--junit-xml=~/cwlexec-0.1.xml' || /bin/true
 
